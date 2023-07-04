517ac66f29f341faa3284a8ce5f0bd59555dcf0a
John <89491969+dededsg@users.noreply.github.com>
2023-07-04 10:15:02 -0300

    27/06

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET home page. */
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: "You're so cool" });
+});
+
 module.exports = router;
